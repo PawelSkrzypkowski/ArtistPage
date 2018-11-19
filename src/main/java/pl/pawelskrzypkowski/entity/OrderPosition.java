698a0df7ae81320e60e57643ac1db678c63702a5
Paweł Skrzypkowski <pawel.skrzypkowski@outlook.com>
2018-11-19 18:46:57 +0100

    Relacje miedzy encjami modelu sklepu

---
+++ src/main/java/pl/pawelskrzypkowski/entity/OrderPosition.java
@@ -0,0 +1,37 @@
+package pl.pawelskrzypkowski.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.Where;
+import pl.pawelskrzypkowski.entity.base.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ * Created by Paweł Skrzypkowski
+ * Wojskowa Akademia Techniczna im. Jarosława Dąbrowskiego, Warszawa 2018.
+ */
+
+@Entity
+@Data
+@Where(clause = "active!=false")
+public class OrderPosition extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "productId")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "orderId")
+    private Order order;
+
+    @Column
+    private Integer count;
+}
---
+++ src/main/java/pl/pawelskrzypkowski/entity/Product.java
@@ -0,0 +1,33 @@
+package pl.pawelskrzypkowski.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.Where;
+import pl.pawelskrzypkowski.entity.base.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ * Created by Paweł Skrzypkowski
+ * Wojskowa Akademia Techniczna im. Jarosława Dąbrowskiego, Warszawa 2018.
+ */
+
+@Entity
+@Data
+@Where(clause = "active!=false")
+public class Product extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private Boolean isUniqueProduct;
+
+    @ManyToOne
+    @JoinColumn(name = "uniqueProductId")
+    private UniqueProduct uniqueProduct;
+}
